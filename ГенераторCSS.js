4af600f4dcea54c2c692e1f9a3c56b297c29f867
OlegSavkin <oleinaTrend@gmail.com>
2025-05-26 00:04:57 +0500

    Добавление разделений на определенные функции в JS

---
+++ ГенераторCSS.js
@@ -0,0 +1,7 @@
+const animationSelect = document.getElementById('animationSelect');
+  const colorPicker = document.getElementById('colorPicker');
+  const speedRange = document.getElementById('speedRange');
+  const speedValue = document.getElementById('speedValue');
+  const animatedBox = document.getElementById('animatedBox');
+  const cssOutput = document.getElementById('cssOutput');
+  const copyButton = document.getElementById('copyButton');
